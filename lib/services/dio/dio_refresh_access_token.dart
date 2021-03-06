306ecd877adc0b6f61788ba436896deb30d13ad0
ismayilov1995 <ismayilov1995@gmail.com>
2021-03-06 15:52:42 +0400

    update interceptor retry

--- lib/services/dio/dio_refresh_access_token.dart
+++ lib/services/dio/dio_refresh_access_token.dart
@@ -16,8 +16,25 @@ class RefreshAccessToken {
       final token = jsonDecode(res.toString())['access_token'];
       onRefreshed(token);
       requestOptions.headers['Authorization'] = 'Bearer: $token';
+      final ops = Options(
+        method: requestOptions.method,
+        sendTimeout: requestOptions.sendTimeout,
+        receiveTimeout: requestOptions.receiveTimeout,
+        extra: requestOptions.extra,
+        headers: requestOptions.headers,
+        responseType: requestOptions.responseType,
+        contentType: requestOptions.contentType,
+        validateStatus: requestOptions.validateStatus,
+        receiveDataWhenStatusError: requestOptions.receiveDataWhenStatusError,
+        followRedirects: requestOptions.followRedirects,
+        maxRedirects: requestOptions.maxRedirects,
+        requestEncoder: requestOptions.requestEncoder,
+        responseDecoder: requestOptions.responseDecoder,
+        listFormat: requestOptions.listFormat,
+      );
       return dio.request(
         requestOptions.path,
+        options: ops,
         data: requestOptions.data,
         cancelToken: requestOptions.cancelToken,
         queryParameters: requestOptions.queryParameters,
