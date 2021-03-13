7b0ea8caf07e003a0bd418aebc8d386bae067254
ismayilov1995 <ismayilov1995@gmail.com>
2021-03-13 16:33:07 +0400

    if unauth back to home

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:culinary_app/data/repositories/repositories.dart';
+import 'package:culinary_app/logic/blocs/auth/authentication_bloc.dart';
 import 'package:culinary_app/logic/cubits/cubits.dart';
 import 'package:culinary_app/presentation/widgets/widgets.dart';
 import 'package:flutter/cupertino.dart';
@@ -10,19 +12,26 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          primary: false,
-          physics: BouncingScrollPhysics(),
-          children: [
-            UserWelcomeRow(),
-            SearchRow(),
-            BlocProvider(
-                create: (_) => RecipeCubit(), child: RecommendRecipesRow()),
-            BlocProvider(
-                create: (_) => RecipeCubit(),
-                child: RecipesListView('Latest Recipes')),
-          ],
+      body: BlocListener<AuthenticationBloc, AuthenticationState>(
+        listener: (context, state) {
+          if (state.status == AuthStatus.unauthenticated) {
+            Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+          }
+        },
+        child: SafeArea(
+          child: ListView(
+            primary: false,
+            physics: BouncingScrollPhysics(),
+            children: [
+              UserWelcomeRow(),
+              SearchRow(),
+              BlocProvider(
+                  create: (_) => RecipeCubit(), child: RecommendRecipesRow()),
+              BlocProvider(
+                  create: (_) => RecipeCubit(),
+                  child: RecipesListView('Latest Recipes')),
+            ],
+          ),
         ),
       ),
     );
